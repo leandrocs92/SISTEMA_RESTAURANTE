720e4b25aff417da176d8210a44c50ad467ac0e8
João Victor <joaovitorcopa@hotmail.com>
2019-06-20 11:43:52 -0300

    Update ProdutoController.java

--- System_Restaurante_Petiscaria_RangoBom/System/src/controler/ProdutoController.java
+++ System_Restaurante_Petiscaria_RangoBom/System/src/controler/ProdutoController.java
@@ -1,13 +1,13 @@
-
 package controler;
+
 import DAO.ProdutoDAO;
 import MODEL.Produto;
 
 public class ProdutoController {
-    
-    public static Produto buscaProdutoCod (int codigo){
+
+    public static Produto buscaProdutoCod(int codigo) {
         //chamar metodo DAO
-      Produto produtoBusca = ProdutoDAO.buscaProduto(codigo);
-      return produtoBusca;
+        Produto produtoBusca = ProdutoDAO.buscaProduto(codigo);
+        return produtoBusca;
     }
 }
